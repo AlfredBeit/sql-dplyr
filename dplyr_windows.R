253c58531925c24a8c4dfc0d805f628bdc90d27d
AlfredBeit <abittaraev@gmail.com>
2019-10-23 01:50:23 +0300

    Add files via upload

--- dplyr_windows.R
+++ dplyr_windows.R
@@ -2,12 +2,46 @@ require(dplyr)
 require(lubridate)
 
 
-data <-
-  read.csv(
-    '/Users/abittaraev/Desktop/sql_dplyr.csv',
-    stringsAsFactors = FALSE,
-    header = TRUE
+
+data <- data_frame(
+  transactions_ts = c(
+    "2016-06-18 13:46:51",
+    "2016-06-18 17:29:25",
+    "2016-06-18 23:07:12",
+    "2016-06-19 07:14:56",
+    "2016-06-19 21:59:40",
+    "2016-06-17 12:39:46",
+    "2016-06-17 20:22:17",
+    "2016-06-20 11:29:02",
+    "2016-06-20 18:59:48"
+    
+  ),
+  user_id = c(rep(13811335, 5),
+              rep(3378024101, 3),
+              13811335),
+  transaction_id = c(
+    1322361417,
+    3729362318,
+    1322363995,
+    7482365143,
+    1322369619,
+    9322351612,
+    9322353031,
+    6928364072,
+    1322375547
+  ),
+  item = c(
+    "glove",
+    "hat",
+    "vase",
+    "cup",
+    "mirror",
+    "dress",
+    "vase",
+    "tie",
+    "mirror"
   )
+)
 
 print(data)
 
